1c0ca6a0070e1f77a33a98b727fe3d8ccdcf3836
ducthanh512 <ducthanh512@gmail.com>
2018-07-16 15:53:41 +1000

    change to aws server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyparser = require('body-parser');
 
 var connection = require('./connection');
 var routes = require('./routes');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 var app = express();
 app.use(function(req, res, next) {
