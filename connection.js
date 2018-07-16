58acea78f15b31cb5bdc47ec93ef4b1ebeffd8d5
ducthanh512 <ducthanh512@gmail.com>
2018-07-16 15:20:44 +1000

    change to aws server

--- connection.js
+++ connection.js
@@ -6,10 +6,16 @@ function Connection() {
   this.init = function() {
     this.pool = mysql.createPool({
       connectionLimit: 10,
-      host: 'us-cdbr-iron-east-04.cleardb.net',
-      user: 'b21da7aaabf93c',
-      password: 'c38472cf',
-      database: 'heroku_1d252267e6e485f'
+      // host: 'us-cdbr-iron-east-04.cleardb.net',
+      // user: 'b21da7aaabf93c',
+      // password: 'c38472cf',
+      // database: 'heroku_1d252267e6e485f'
+
+      host: 'musicappdb.caetv9pd8to1.us-east-1.rds.amazonaws.com',
+      user: 'root',
+      password: '12345678',
+      database: 'MusicAppDB'
+
     });
   };
   
